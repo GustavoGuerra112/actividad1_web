c19b08b4eeb565252aac16a9ac1202d4163a26f7
GustavoGuerra112 <l23211976@tectijuana.edu.mx>
2026-02-16 05:48:43 +0000

    Cambios

--- web.py
+++ web.py
@@ -10,11 +10,30 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
+        if self.url.path == '/':
+            archivo =open('home.html')
+            html=archivo.read()
+            self.send_response(200)
+            self.send_header("Content-Type","text/html")
+            self.end_headers()
+            self.wfile.write(html).endcode("utf-8")
 
+        if self.valida.autor():    
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            self.wfile.write(self.get_html(self.url().path,self.querydata()).encode("utf-8"))
+        else:
+            self.send_error(404,'El autor no existe')
+    
+    def valida_autor(self):
+        if 'autor' in self.query_data():
+            return True
+    def get_html(self, path, qs):
+    
+        return f"""
+        <h1>Proyecto: {path} Autor: {qs['autor']} </h1>
+"""
     def get_response(self):
         return f"""
     <h1> Hola Web </h1>
